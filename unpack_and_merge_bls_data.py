a5b02f9bcd7a2667af94ca84d9e1680abfc20e7e
nmackler <108547483+nmackler@users.noreply.github.com>
2023-08-20 07:43:12 -0700

    Initial Commit
    Downloaded and extracted all BLS excel tables containing data, downloaded Occupation Code definitions and created CSV containing all CDL driver occupation codes. Also began framework for data cleaning/extraction of wage data.

---
+++ unpack_and_merge_bls_data.py
@@ -0,0 +1,26 @@
+import os
+import pandas as pd
+
+# Path to the folder containing the Excel files
+folder_path = 'C:\Users\\natha\OneDrive\Documents\Marijuana\BLS Excels'
+
+# Get a list of all files in the folder
+file_list = os.listdir(folder_path)
+
+# Create a dictionary to store DataFrames
+dataframes = {}
+
+# Loop through the files
+for file_name in file_list:
+    if file_name.endswith('.xlsx') or file_name.endswith('.xls'):  # Check if the file is an Excel file
+        # Extract year from the file name (necessary becuase the BLS is inconsistent with file naming)
+        numbers = ''.join(filter(str.isdigit, file_name))
+        
+        # Create DataFrame
+        df = pd.read_excel(os.path.join(folder_path, file_name))
+        
+        # Store DataFrame in the dictionary
+        dataframes[numbers] = df
+
+
+
